c702a75d62245b27f63d2a391a4388a7787f6825
Neeraj gaur <neerajgaur8448@gmail.com>
2026-03-16 18:34:36 +0530

    feat: initialize backend server with Express, Socket.io, MongoDB, and essential middleware.

--- backend/index.ts
+++ backend/index.ts
@@ -40,7 +40,7 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("common"));
 }
 app.use(compression());
-app.use(helmet());
+// app.use(helmet());
 app.use(
   cors({
     origin: process.env.CLIENT_URL || "http://localhost:3000",
